12f7229b55dd7c606f9f7babf26756574123e9ff
Umagreeg-40 <umamahesankaranss@gmail.com>
2022-01-26 17:01:03 -0800

    Third project

---
+++ 10-01-2022/src/com/javaBasics/AboutProduct.java
@@ -0,0 +1,25 @@
+package com.javaBasics;
+
+public class AboutProduct {
+
+	public static void main(String[] args) {
+		
+		int productId=107563;
+		String productName= "Johnsons Baby Milk Path";
+		int productPrice=1250;
+		char productGrade='A';
+		String productManufacture="Johnson and Johnson";
+		String manufactureAddress="Mumbai";
+		
+		System.out.println(productName);
+		System.out.println(productId);
+		System.out.println(productPrice);
+		System.out.println(productGrade);
+		System.out.println(productManufacture);
+		System.out.println(manufactureAddress);
+		
+		
+
+	}
+
+}
